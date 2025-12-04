d51838c76280ec639e78924d3ba5b4e6e1ad4e92
MonsOnc61 <zhongyuye2001@gmail.com>
2025-12-04 12:19:32 +0100

    change fundamental conception of patient_info.py, add clinical_parser. BE CAREFUL, I didn't add clinical_data.csv in the repository so if sb want to test the functionality, he/she should put the right path for parse_scv !!!

--- contexte/patient_info.py
+++ contexte/patient_info.py
@@ -1,27 +1,40 @@
-class PatientInfo:
+class TimepointInfo:
     def __init__(self, 
-                 id: str | None = None, 
+                 timepoint: str | None = None, 
                  age: float | None = None,
-                 sex: str | None = None, 
-                 ms_type: str | None = None,
                  edss: float | None = None, 
+                 ms_type: str | None = None,
                  lesion_number: int | None = None,
                  lesion_volume: float | None = None):
-        self.id = id
+        self.timepoint = timepoint
         self.age = age
-        self.sex = sex
         self.ms_type = ms_type
         self.edss = edss
         self.lesion_number = lesion_number
         self.lesion_volume = lesion_volume
-        
+    
     def __str__(self):
         return f"""
-id = {self.id},
+timepoint = {self.timepoint},
 age = {self.age},
-sex = {self.sex},
 ms_type = {self.ms_type},
 edss = {self.edss},
 lesion_number = {self.lesion_number},
 lesion_volume = {self.lesion_volume}
+"""
+
+class PatientInfo:
+    def __init__(self, 
+                 id: str | None = None, 
+                 sex: str | None = None, 
+                 timepoint_infos: dict[str: TimepointInfo] | None = None):
+        self.id = id
+        self.sex = sex
+        self.timepoint_infos = timepoint_infos
+        
+    def __str__(self):
+        return f"""
+id = {self.id},
+sex = {self.sex},
+timepoint_infos = {self.timepoint_infos},
 """

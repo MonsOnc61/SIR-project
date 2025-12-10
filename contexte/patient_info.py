b6054d2f0dae6e8fd0f169acdc2ac194d13c99af
MonsOnc61 <zhongyuye2001@gmail.com>
2025-12-10 19:35:19 +0100

    add timepoint_counter for PatientInfo

--- contexte/patient_info.py
+++ contexte/patient_info.py
@@ -27,14 +27,17 @@ class PatientInfo:
     def __init__(self, 
                  id: str | None = None, 
                  sex: str | None = None, 
+                 timepoint_counter: int = 0,
                  timepoint_infos: dict[str: TimepointInfo] | None = None):
         self.id = id
         self.sex = sex
+        self.timepoint_counter = timepoint_counter
         self.timepoint_infos = timepoint_infos
         
     def __str__(self):
         return f"""
 id = {self.id},
 sex = {self.sex},
+timepoint_number = {self.timepoint_counter},
 timepoint_infos = {self.timepoint_infos},
 """
